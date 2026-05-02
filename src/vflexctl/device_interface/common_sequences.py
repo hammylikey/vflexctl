618dd3bd02184f60ef0b4c594f7aa329a319dab2
hameedm <hmohammed43@icloud.com>
2026-05-02 16:55:49 +0100

    Switch to the MIDITriplet type to better describe CommandList (vs VFlexProtoMessage)

--- src/vflexctl/device_interface/common_sequences.py
+++ src/vflexctl/device_interface/common_sequences.py
@@ -4,10 +4,12 @@ made with command_preparation.
 """
 
 from vflexctl.protocol import VFlexProto, prepare_command_for_sending, prepare_command_frame
+from vflexctl.types import MIDITriplet
 
 __all__ = ["GET_VOLTAGE_SEQUENCE", "GET_LED_STATE_SEQUENCE", "GET_SERIAL_NUMBER_SEQUENCE"]
 
-type CommandList = list[tuple[int, int, int]]
+
+type CommandList = list[MIDITriplet]
 
 GET_SERIAL_NUMBER_SEQUENCE: CommandList = prepare_command_for_sending(
     prepare_command_frame([VFlexProto.CMD_GET_SERIAL_NUMBER])
